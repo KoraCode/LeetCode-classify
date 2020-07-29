bb3d5c64c57303d12e96eb62a8d7411dbbacc75d
KoraCode <62596768+KoraCode@users.noreply.github.com>
2020-07-29 14:04:49 +0800

    Create First9.cpp

---
+++ First9.cpp
@@ -0,0 +1,11 @@
+题目：给定一个二叉树，找出其最大深度。二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+
+解法：树的深度等于max（左子树的深度，右子树深度）+1
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (root == nullptr) return 0;
+        return max(maxDepth(root->left), maxDepth(root->right)) + 1;
+    }
+};
