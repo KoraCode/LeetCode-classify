8e38e632f117e26f9c82acc7fa4ae182dd16a4ef
KoraCode <62596768+KoraCode@users.noreply.github.com>
2020-08-06 13:43:01 +0800

    Create Second8.cpp

---
+++ Second8.cpp
@@ -0,0 +1,18 @@
+题目：给你一个整数数组 nums ，请你找出数组中乘积最大的连续子数组（该子数组中至少包含一个数字），并返回该子数组所对应的乘积。
+
+解法：使用一个max跟min区分正负数的关系
+
+//使用滚动数组优化了空间使用率
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int maxF = nums[0], minF = nums[0], ans = nums[0];
+        for (int i = 1; i < nums.size(); ++i) {
+            int mx = maxF, mn = minF;
+            maxF = max(mx * nums[i], max(nums[i], mn * nums[i]));
+            minF = min(mn * nums[i], min(nums[i], mx * nums[i]));
+            ans = max(maxF, ans);
+        }
+        return ans;
+    }
+};
