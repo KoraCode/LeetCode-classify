4262445b46d6d5c9e49ade68dcf700f134ef4617
KoraCode <62596768+KoraCode@users.noreply.github.com>
2020-08-08 10:29:12 +0800

    Create Offer11.cpp

---
+++ Offer11.cpp
@@ -0,0 +1,24 @@
+题目：把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。输入一个递增排序的数组的一个旋转，输出旋转数组的最小元素。
+
+解法：二分查找，mid比end的数大，start等于mid+1，mid比end的数小，end等于mid
+
+class Solution {
+public:
+    int minArray(vector<int>& numbers) {
+        int low = 0;
+        int high = numbers.size() - 1;
+        while (low < high) {
+            int pivot = low + (high - low) / 2;
+            if (numbers[pivot] < numbers[high]) {
+                high = pivot;
+            }
+            else if (numbers[pivot] > numbers[high]) {
+                low = pivot + 1;
+            }
+            else {
+                high -= 1;
+            }
+        }
+        return numbers[low];
+    }
+};
