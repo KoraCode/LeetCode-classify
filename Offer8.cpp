d63481568cb353ef1f29ff67e29a1d90bd9a4b62
KoraCode <62596768+KoraCode@users.noreply.github.com>
2020-08-08 09:48:15 +0800

    Create Offer8.cpp

---
+++ Offer8.cpp
@@ -0,0 +1,37 @@
+题目：二个栈实现队列
+
+解法：使用一个栈装插入数，一个栈用于删除
+
+class CQueue {
+    stack<int> stack1,stack2;
+public:
+    CQueue() {
+        while (!stack1.empty()) {
+            stack1.pop();
+        }
+        while (!stack2.empty()) {
+            stack2.pop();
+        }
+    }
+    
+    void appendTail(int value) {
+        stack1.push(value);
+    }
+    
+    int deleteHead() {
+        // 如果第二个栈为空
+        if (stack2.empty()) {
+            while (!stack1.empty()) {
+                stack2.push(stack1.top());
+                stack1.pop();
+            }
+        } 
+        if (stack2.empty()) {
+            return -1;
+        } else {
+            int deleteItem = stack2.top();
+            stack2.pop();
+            return deleteItem;
+        }
+    }
+};
