b9674239d1ca594dc918089fbec0f53e154a1b86
KoraCode <62596768+KoraCode@users.noreply.github.com>
2020-08-14 09:44:21 +0800

    Update Offer16.cpp

--- Offer16.cpp
+++ Offer16.cpp
@@ -8,7 +8,7 @@ public:
         double res=1.0;
         int i=n;
         while(i){
-            if(i&1)res*=x;  //i的低位存在，res*x
+            if(i&1)res*=x;  //i的低位存在，res*x      用于判断n为奇数还是偶数
             x*=x;           //x扩大为它的平方，因为二进制每位的差距是平方关系
             i/=2;           //i向0靠近
         }
